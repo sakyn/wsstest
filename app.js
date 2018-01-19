81d054f6ad0c535e106c14c28726dff48eaf5125
Jiří Sakáč <ct@sakac.cz>
2018-01-19 17:27:29 +0100

    x

--- app.js
+++ app.js
@@ -1,72 +1,77 @@
-const httpsEnabled = true;
-const socketLimit = 500000;
-
 let express = require('express');
 let app = express();
-let http = require('http').createServer();
 let https = require('https');
 let fs = require('fs');
+let request = require('request');
 
 let sio = require('socket.io');
 let _ = require('lodash');
 let bodyParser = require('body-parser');
+let cio = require('socket.io-client');
 
+let props, server, pk, crt, ca;
 
-let server, pk, crt, ca;
+pk = fs.readFileSync('./certs/private.key').toString();
+crt = fs.readFileSync('./certs/ct.crt').toString();
+ca = fs.readFileSync('./certs/ca.crt').toString();
 
-if(httpsEnabled){
+let iId = typeof process.env.NODE_APP_INSTANCE === "undefined" ? 1 : (parseInt(process.env.NODE_APP_INSTANCE) + 1);
 
-	pk = fs.readFileSync('./certs/private.key').toString();
-	crt = fs.readFileSync('./certs/ct.crt').toString();
-	ca = fs.readFileSync('./certs/ca.crt').toString();
+server = https.createServer({key: pk, cert: crt, ca: ca }, app);
+server.listen((8000 + iId), '0.0.0.0');
 
-	server = https.createServer({key:pk, cert:crt, ca:ca }, app);
-	server.listen(9443, '0.0.0.0');
-
-	console.log("Starting secure WSS server, listening on port 9443");
-}else{
-	server = http.createServer(app);
-	server.listen(9080, '0.0.0.0');
-
-	console.log("Starting non-secure WS server, listening on port 9080");
-}
+props = { numSockets: '1', dataFrequency: 1000 };
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
-
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	next();
 });
 
+let instanceId = iId + "-" + new Array(10).fill("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+	.map(function(x) { return x[Math.floor(Math.random() * x.length)] }).join('');
 
-let io = sio.listen(server,{key:pk,cert:crt,ca:ca});
+console.log("Starting secure WSS server, listening on port "+800 + iId+", ID: " + instanceId);
 
-let events = [];
 let eventCount = 0;
-let stats = [0];
 let errors = [];
 
-let props = { numSockets: '1', dataFrequency: 1000 };
+
+
+let io = sio.listen(server,{key: pk, cert: crt, ca: ca, secure: true, rejectUnauthorized: false});
 
 
 /**
  * Separate NSP
  */
-let dashboard = io.of('/dashboard');
+let backend = cio.connect('https://wssdashboard.devct.cz/backend', {reconnect: true, secure: true, rejectUnauthorized: false});
+
+backend.on("connect", function(){
+	console.log("Backend connected...")
+});
+
+backend.on('command:changeProps', function (d) {
+	props = d;
+	io.emit("command:changeProps", d);
+	console.log(d)
+});
+
+backend.on('command:killKenny', function () {
+	console.log("command:killKenny");
+	io.emit("command:killKenny");
+});
 
 /**
  * Update dashboard
  */
 let update = function(){
-	dashboard.emit("update", {
+	backend.emit("update", {
+		instanceId: instanceId,
 		totalClients: io.engine.clientsCount,
 		wsClients: _.filter(io.sockets.connected, function(socket){ return socket.conn.transport.name === "websocket"; }).length,
-		activeClients: getActiveClients(),
-		stats: stats,
 		errors: errors,
 		eventsCount: eventCount
 	})
@@ -81,14 +86,6 @@ app.post('/', function (req, res) {
 	res.send("ok");
 });
 
-
-/**
- * Backend
- */
-app.get('/dashboard',function(req,res){
-	res.sendFile(__dirname + '/index.html');
-});
-
 /**
  * test frontend
  */
@@ -96,38 +93,7 @@ app.get('/test',function(req,res){
 	res.sendFile(__dirname + '/front.html');
 });
 
-/**
- * Test
- */
-setInterval(function(){
-	update();
-	dashboard.emit("props", props);
-}, 1000);
 
-/**
- * Snip number of clients
- */
-setInterval(function(){
-	stats.push(io.engine.clientsCount);
-	stats.splice(10);
-}, 6000);
-
-
-/**
- * Filter clients, active in last 5s
- * @returns {Number}
- */
-let getActiveClients = function(){
-	let ids = [];
-	_.each(events, function(event){
-		if(ids.indexOf(event.id) > -1) return false;
-		if(event.time >= (+ new Date() - 5000)){
-			ids.push(event.id);
-		}
-	});
-
-	return ids.length;
-};
 
 /**
  *
@@ -136,26 +102,19 @@ let getActiveClients = function(){
  */
 let addEvent = function(e, socket){
 	eventCount++;
-	/**
-	dashboard.emit("evt", {
-		lastEvent: _.extend(e, {time: + new Date(), id: socket.id, ip: socket.request.connection.remoteAddress}),
-		eventsCount: eventCount,
-	})
-	 */
 };
+
+setInterval(function(){
+	update();
+}, 1000);
+
 
 /**
  * Bind events
  */
 io.on('connection', function (socket) {
 
-	if(io.engine.clientsCount >= socketLimit) {
-		socket.disconnect();
-		addEvent({type: "Disconnect", value: "MAX LIMIT"}, socket);
-	}
-
-	//addEvent({type: "Connect", value: ""}, socket);
-	//update();
+	addEvent({type: "Connect", value: ""}, socket);
 
 	socket.on('clientEvent', function(evt){
 		addEvent(evt, socket);
@@ -170,29 +129,6 @@ io.on('connection', function (socket) {
 	});
 
 	socket.emit("command:changeProps", props);
-
-});
-
-
-//Dashboard connection, send stats
-dashboard.on('connection', function (socket) {
-
-	socket.emit("props", props);
-
-	update();
-
-	socket.on('command:changeProps', function (d) {
-		props = d;
-		io.emit("command:changeProps", d)
-		console.log(d)
-	});
-
-	socket.on('command:killKenny', function () {
-		console.log("command:killKenny");
-		props = { numSockets: '1', dataFrequency: 1000 };
-		io.emit("command:killKenny");
-	});
-
 });
 
 
